3c8031a109dc6cdeacb1485a9971de2b00a723b4
esteban-vm <estebanvm90@outlook.com>
2024-02-06 09:05:44 -0300

    add detail page

---
+++ src/app/detail/[city_id]/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Detail() {
+  return (
+    <main>
+      <div className='container'>
+        <Link href='/'>&larr; Home</Link>
+      </div>
+    </main>
+  )
+}
